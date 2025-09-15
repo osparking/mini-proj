e7cef918e66803cd6e750430bdbf03f9804a0231
BumTiger <jbpark03@gmail.com>
2025-09-15 11:21:00 +0900

    make replaceAll usable by ES2021

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ function calculateSalary(hoursTaught: number) {
 }
 
 console.log(`월급 : ${calculateSalary(20 * 8).toLocaleString()}`);
+
+const myDoc: Document = document;
+console.log(document.URL);
+const button = document.getElementById('btn');
+console.log(button);
+
+let modified = "hello".replaceAll("l", "L");
+console.log(modified);

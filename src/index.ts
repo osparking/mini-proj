e9ca5a743bedc48e18db69a3f6a1f228b45bdd4e
BumTiger <jbpark03@gmail.com>
2025-09-15 13:13:35 +0900

    use type assertion and non-null assertion

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ console.log(`월급 : ${calculateSalary(20 * 8).toLocaleString()}`);
 
 const myDoc: Document = document;
 console.log(document.URL);
-const button = document.getElementById('btn');
-console.log(button);
 
-let modified = "hello".replaceAll("l", "L");
-console.log(modified);
+let content: unknown = "나는 보통비누를 5 개 구매하였다.";
+const contentLength: number = (content as string).length;
+
+const button = document.getElementById("btn")!;
+button.addEventListener("click", () => {
+  alert(todoInput.value);
+  todoInput.value = "";
+});
+
+let todoInput: HTMLInputElement = document.getElementById(
+  "todoinput"
+) as HTMLInputElement;

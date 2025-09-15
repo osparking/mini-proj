c661d355989d64dcf903a65c8612551a122631f1
BumTiger <jbpark03@gmail.com>
2025-09-15 15:00:39 +0900

    prevent 'refresh' - default form submission behavior

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const form = document.querySelector("form")!;
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function(e){
+  e.preventDefault();
   console.log("제출됨");
 });

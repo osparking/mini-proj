7a2cd14c0abab6333cf71158a8201bb2a3afaa32
BumTiger <jbpark03@gmail.com>
2025-09-15 15:38:16 +0900

    append new todo item onto the list

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 const form = document.querySelector("form")!;
+const todolist = document.getElementById("todolist")!;
+const todoinput = document.getElementById("todoinput");
+const input = document.querySelector("#todoinput")! as HTMLInputElement;
 
 function submitForm (e : SubmitEvent){
   e.preventDefault();
-  console.log("제출됨");
+  const newTodo = input.value;
+  const newLi = document.createElement("li");
+  newLi.append(newTodo);
+  todolist.append(newLi);
+  input.value = '';
 }
 
 form.addEventListener("submit", submitForm);
